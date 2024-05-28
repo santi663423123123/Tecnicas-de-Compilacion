d89ac33e497370aa6f7a4cd0f3f4f6fed650b246
santi663423123123 <santiagoarteta@gmail.com>
2024-05-28 12:31:32 -0300

    version final

---
+++ src/main/java/compiladores/App.java
@@ -0,0 +1,37 @@
+package compiladores;
+
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+import java.io.IOException;
+
+public class App {
+    public static void main(String[] args) throws IOException {
+        String fileName = "../test.c";
+        try {
+            CharStream input = CharStreams.fromFileName(fileName);
+            compiladoresLexer lexer = new compiladoresLexer(input);
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            compiladoresParser parser = new compiladoresParser(tokens);
+            ParseTree tree = parser.programa(); // Asegúrate de que la regla de inicio es 'programa'
+    
+            System.out.println(tree.toStringTree(parser));
+
+            Caminante2 visitor = new Caminante2();
+            visitor.visit(tree);
+            System.out.println(visitor);
+            System.out.println(visitor.getErrorNodes());
+
+            Escucha2 escucha = new Escucha2();
+            ParseTreeWalker walker = new ParseTreeWalker();
+            walker.walk(escucha, tree);
+
+  
+            
+        } catch (final IOException e) {
+            System.err.println(e);
+        }
+    }
+}
+
+
+
